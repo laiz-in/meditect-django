257457430a3b36c65bf64031196dfb1d1c5a992f
laiz-in <laizin2107@gmail.com>
2023-07-26 01:36:38 +0400

    added alzheimers page

--- meditect/myapp/views.py
+++ meditect/myapp/views.py
@@ -23,8 +23,24 @@ def ReadMoreAboutAI(request):
 def skindisease(request):
    return render(request, 'skindisease.html')
 
+
+
 def alzheimers(request):
+   if request.method == 'POST' and request.FILES.get('image'):
+         # Get the uploaded image from the request
+         uploaded_image = request.FILES['image']
+         
+         # Process the image (your image processing logic goes here)
+         # For demonstration purposes, let's assume the result is a simple text string
+         result_text = "This is the result of image processing."
+
+         # Send the result_text as a response
+         return render(request, 'alzheimers.html', {'result_text': result_text})
+
+      # If it's a GET request or no image was uploaded yet, render the empty form
    return render(request, 'alzheimers.html')
+
+
 
 def ReadMoreAboutAI(request):
    return render(request, 'ReadMoreAboutAI.html')
